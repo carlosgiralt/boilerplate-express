b6995c65ca710171d163539f79f61489379e0f61
Carlos Giralt <cagiraltt@gmail.com>
2021-01-05 19:06:53 +0000

    use env vars

--- myApp.js
+++ myApp.js
@@ -3,14 +3,19 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
 app.get('/json', (req, res) => {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+
   res.send({
-    "message": "Hello json"
+    "message": message
   });
 })
 
