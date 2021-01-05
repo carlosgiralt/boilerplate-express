09f6886aee0b41c3e54331ebc6f68704b1669e08
Carlos Alberto Giralt Torriente <cagiraltt@gmail.com>
2021-01-05 13:11:50 -0500

    serve an html file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 
 app.get('', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 
